8e3b5289330529f4a01b9b3a4050d7ccb9ac7b0d
Boyana15 <bojana.iw@gmail.com>
2024-03-31 18:48:40 +1100

    Initial Code

---
+++ JS/index.js
@@ -0,0 +1,37 @@
+console.log ("JS loaded correctly");
+
+
+
+// Scroll to About Me Section
+$("#about").on("click",function() {
+    $("html, body").animate({scrollTop: $(".heroImg").offset().top
+}, 2000);
+});
+
+// Scroll to Skills Section
+$("#skills").on("click",function() {
+    $("html, body").animate({scrollTop: $(".mySkills").offset().top
+}, 2000);
+});
+
+// Scroll to myWork Section
+$("#work").on("click",function() {
+    $("html, body").animate({scrollTop: $(".myWork").offset().top
+}, 2000);
+});
+
+//Nav Hover
+$( "a" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+    console.log ("Nav hover triggered");
+  });
+
+  //Button Hovers
+  $( "button" ).hover(function() {
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn( 500 );
+    console.log ("Button hover triggered");
+  });
+
+  
